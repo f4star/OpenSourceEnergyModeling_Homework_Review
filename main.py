36576b523a3a47d8668cba71ff2a52ec8a8a4c52
xtsabix <vadaszcsaba2000@gmail.com>
2025-03-23 21:49:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 from utils import square, cube, is_even
 
 def main():
-    while True:
+    while True: #end while when letter entered
         num = input("Enter a number (or any letter to EXIT): ")
         if not num.isdigit():
             print("EXIT program.")
-            break
+            break #ending program
         num = int(num)
         print(f"Square: {square(num)}")
         print(f"Cube: {cube(num)}")
